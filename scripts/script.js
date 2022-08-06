b9e76a654b27c2d89c8ed4e5c89c3571c1a1ef3c
playertt <playertt@gmail.com>
2022-08-06 18:05:50 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+const textoParaCriptografar = document.getElementById('textoparacriptografar');
+const textoParaDecodificar = document.getElementById('textoparadecodificar');
+const textAreaResultado = document.getElementById('textarea-resultado');
+
+function criptografar(textoParaCriptografar){
+
+    textoParaCriptografar = textoParaCriptografar.value;
+    let textoCodificado = textoParaCriptografar.split("");
+    for(var i = 0; i <= textoCodificado.length; i++){
+        if(textoCodificado[i] == "a"){
+            textoCodificado[i] = "ai";
+        }else if(textoCodificado[i] == "e"){
+            textoCodificado[i] = "enter";
+        }else if(textoCodificado[i] == "i"){
+            textoCodificado[i] = "imes";
+        }else if(textoCodificado[i] == "o"){
+            textoCodificado[i] = "ober";
+        }else if(textoCodificado[i] == "u"){
+            textoCodificado[i] = "ufat";
+        }
+    }
+    textoCodificado = textoCodificado.join("");
+    textAreaResultado.value = textoCodificado;
+
+}
+
+function decodificar(textoParaCriptografar){
+
+    textoParaCriptografar = textoParaCriptografar.value;
+
+    textoParaCriptografar = textoParaCriptografar.replace(/enter/g,"e");
+    textoParaCriptografar = textoParaCriptografar.replace(/imes/g,"i");
+    textoParaCriptografar = textoParaCriptografar.replace(/ai/g,"a");
+    textoParaCriptografar = textoParaCriptografar.replace(/ober/g,"o");
+    textoParaCriptografar = textoParaCriptografar.replace(/ufat/g,"u");
+    
+    textAreaResultado.value = textoParaCriptografar;
+
+}
+
+function desabilitarBotaoCopiar() {
+    if(
+        (document.getElementById("textoparacriptografar").value==="")&&
+        (document.getElementById("textarea-resultado").value==="")) { 
+        document.getElementById('botaocopiar').disabled = true; 
+    } else { 
+        document.getElementById('botaocopiar').disabled = false;
+    }
+}
+
+function copyToClipBoard() {
+
+    let textoCopiado = document.getElementById("textarea-resultado");
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(textoCopiado.value);
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Texto Copiado: " + textoCopiado.value;
+
+}
+
+function limparResultado(){
+    
+    textAreaResultado.value = "";
+
+}
+
+function outFunc() {
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copiar para clipboard";
+
+}
